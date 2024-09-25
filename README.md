d7473a039eef541de969bebf85459384ddd74091
Namo <ciderheist@gmail.com>
2024-09-25 17:02:51 +0100

    push

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Web Dev Project Par Cars
+
+This is a project for our Web Dev Class, it is a small website that showcase Car Collection and tag images to help people find people cars
+
+We will go through and add in the ability to switch themes so visually impaired people can change it for their needs, format the HTML correctly for screen readers, make the text blocky and big for people with impaired vision and ALT text on images for screen readers
--- public/script.js
+++ public/script.js
@@ -13,6 +13,34 @@ const hotWheels = [
         features: ["cartoon", "humor"],
         image: "resources/Cars/BeavisCar.png"
     },
+    {
+        name: "Black Car",
+        color: "black",
+        wheels: 4,
+        features: ["NA"],
+        image: "resources/Cars/BlackCar.png"
+    },
+    {
+        name: "Black Car",
+        color: "black",
+        wheels: 4,
+        features: ["NA"],
+        image: "resources/Cars/BlackCar2.png"
+    },
+    {
+        name: "Black Sports Car",
+        color: "black",
+        wheels: 4,
+        features: ["Sports Car"],
+        image: "resources/Cars/BlackSportsCar.png"
+    },
+    {
+        name: "Blue Jeep",
+        color: "blue",
+        wheels: 4,
+        features: ["NA"],
+        image: "resources/Cars/BlueJeep.png"
+    },
     // Gotta add more cars but lazy
 ];
 
